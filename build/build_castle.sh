dad15ba950b8fe2d06ee9844bd7993cd451b077b
Surasak Prommarat <prommarats@gmail.com>
2016-06-15 17:02:32 +0700

    no message

--- build/build_castle.sh
+++ build/build_castle.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
-nvm use v5.5.0
 cd ../app
 meteor build --release --directory ../build
-cd ../build/bundle/programs/server
-npm install
